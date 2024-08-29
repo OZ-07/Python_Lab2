e2dac395c3490f2e0e408ed2fc26092310d7bed5
toshi <toshiroozawa04@gmail.com>
2024-08-29 11:36:20 -0700

    finish lab 1

---
+++ Lab1.py
@@ -0,0 +1,17 @@
+import math
+a=math.pi*5**2
+print(a)
+v=(4/3)*math.pi*3**3
+print(v)
+c=math.sqrt(4**2+3**2)
+print(c)
+name='toshiro ozawa'
+print(len('toshiro ozawa'))
+print('toshiro ozawa'.upper())
+print('TOSHIRO OZAWA'.lower())
+print(name.upper())
+A=19
+h=(5*12)+6
+w=185
+BMI=w/h**2*703
+print(BMI)
