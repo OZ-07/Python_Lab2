0b99fb6ce827f42f5092d125f0b600c0eceb1600
toshi <toshiroozawa04@gmail.com>
2024-08-29 12:02:34 -0700

    finish lab 1

--- Lab1.py
+++ Lab1.py
@@ -1,15 +1,20 @@
 import math
+#circle 1
 a=math.pi*5**2
 print(a)
+#circle volume
 v=(4/3)*math.pi*3**3
 print(v)
+#hypotenuse
 c=math.sqrt(4**2+3**2)
 print(c)
+#name
 name='toshiro ozawa'
 print(len('toshiro ozawa'))
 print('toshiro ozawa'.upper())
 print('TOSHIRO OZAWA'.lower())
 print(name.upper())
+#BMI
 A=19
 h=(5*12)+6
 w=185
